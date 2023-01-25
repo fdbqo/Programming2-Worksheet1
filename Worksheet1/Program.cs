6bf641e418a1c52c632863b8f4bc6cacc5ed3e40
Finn De Bie <S00238716@atu.ie>
2023-01-25 10:11:44 +0000

    update 1

--- Worksheet1/Program.cs
+++ Worksheet1/Program.cs
@@ -1,10 +1,47 @@
-﻿namespace Worksheet1
+﻿/*
+ 
+ * Name : Finn De Bie
+ * Date : 25 - 1 - 23
+ * Desc : Revision
+
+ */
+
+using System;
+
+namespace Worksheet1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            PrintIntegersFor();
+
+            Console.WriteLine();
+
+            PrintIntegersWhile();
+
+
+
+        }
+
+        static void PrintIntegersFor()
+        {
+            for (int i = 40; i <= 60; i++)
+            {
+                Console.Write($"{i}, ");
+            }
+
+        }
+
+        static void PrintIntegersWhile()
+        {
+            int i = 40;
+
+            while (i >= 40 && i <= 60)
+            {
+                Console.Write($"{i}, ");
+                i++;
+            }
         }
     }
 }
