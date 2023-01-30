6bdd18e93c260920b42b7a6d483a8baaafa23a61
Finn De Bie <S00238716@atu.ie>
2023-01-30 09:06:24 +0000

    30 / 1 / 23

---
+++ Q2/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Q2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CountOddNumbers();
+        }
+
+        static void CountOddNumbers()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write($"Enter integer {i} : ");
+                int test = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
